eb57a0bf33987521906ac95ea8b6253d993a9393
JonasSchmalzhaf <jonas.schmalzhaf@stackit.cloud>
2025-03-06 09:10:41 +0100

    Feat: Created Endpoints in API-Go server.go

--- API-Go/cmd/server/server.go
+++ API-Go/cmd/server/server.go
@@ -1,1 +1,45 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type NewDB struct {
+	Name string `json:"name"`
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/db-management/databases", getMultipleDBs)
+	router.GET("/db-management/databases/:id", getSingleDBs)
+	router.POST("/db-management/databases", createSingleDB)
+	router.PUT("/db-management/databases/:id", updateSingleDB)
+	router.Run("localhost:8080")
+}
+
+func getMultipleDBs(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, "Multiple DBs")
+}
+
+func getSingleDBs(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, "Single DB: "+c.Param("id"))
+}
+
+func createSingleDB(c *gin.Context) {
+	var newDB NewDB
+
+	if err := c.ShouldBindJSON(&newDB); err != nil {
+	}
+
+	c.IndentedJSON(http.StatusCreated, newDB)
+}
+
+func updateSingleDB(c *gin.Context) {
+	var newDB NewDB
+
+	if err := c.ShouldBindJSON(&newDB); err != nil {
+	}
+
+	c.IndentedJSON(http.StatusOK, "Single DB: "+c.Param("id")+" -> "+newDB.Name)
+}
